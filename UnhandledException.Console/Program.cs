29b904d7bf45b4d355d79113c5bc9599b7dbe539
EUIJUN HAN <hanuijun59@gmail.com>
2017-08-19 00:41:33 -0400

    Add project files.

---
+++ UnhandledException.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnhandledException.Core;
+
+namespace UnhandledException.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+#endif
+            DoSomething();
+        }
+
+        private static void DoSomething()
+        {
+            throw new NotImplementedException("Exception from DoSomthing()");
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var errorMessage = (e.ExceptionObject as Exception).GetInnerExceptionMessage();
+
+            System.Console.WriteLine(errorMessage);
+        }
+    }
+}
---
+++ UnhandledException.Core/ExtensionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnhandledException.Core
+{
+    public static class ExtensionHelper
+    {
+        public static string GetInnerExceptionMessage(this Exception e)
+        {
+            return e.Message + e.InnerException?.GetInnerExceptionMessage();
+        }
+    }
+}
